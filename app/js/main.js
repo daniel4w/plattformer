5d71f99f4d061fca84c8195fd997c7a083b215c2
daniel4w <daniel4w@gmx.de>
2018-02-14 21:27:42 +0100

    change main for testing Particle a bit

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,28 @@
 require(__dirname + "/../css/style.scss");
 import Vec from './math/Vector2d';
+import Particle from './particles/Particle';
 
 let canvas = document.getElementById('screen'),
-    ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d'),
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight;
 
-let vec1 = new Vec(5, 10);
-let vec2 = new Vec(15, 12);
+let vec1 = new Vec(245, 110);
 
-let vec3 = Vec.substract(vec1, vec2);
-vec3.subtractVector
-console.log(vec3);
+let p = new Particle(vec1, 22);
+console.log(p);
+console.log(typeof(ctx));
+p.setFillColor('green');
+p.setBorderColor('red');
+p.setBorderWidth(15);
+p.setVelocity(new Vec(0.1, 0.1));
+function update()
+{
+    ctx.clearRect(0, 0, width, height);
+    p.draw(ctx);
+    requestAnimationFrame(update);
+}
+
+update();
+
+// TODO: write test for Particle.js
